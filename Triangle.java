8e5be275f07041f6812902060d6be1e6de15555c
angNavarro <129991694+angNavarro@users.noreply.github.com>
2024-02-17 06:42:49 +0100

    Update Triangle.java
    Añadidas excepciones individuales

--- Triangle.java
+++ Triangle.java
@@ -8,15 +8,23 @@ public class Triangle {
     double hipotenusa = 0;
 
     public Triangle(double cateto1, double cateto2, double hipotenusa) throws TriangleImposibleException {
-        if (cateto1 + cateto2 <= hipotenusa || cateto1 + hipotenusa <= cateto2 || cateto2 + hipotenusa <= cateto1) { // Definicion
+        if (cateto1 + cateto2 <= hipotenusa) { // Definicion caso 1
             throw new TriangleImposibleException(
-                    "\nTriangle impossible: La suma de dos costats del triangle sempre ha de ser major que la mesura del tercer costat.");
+                    "\nTriangle impossible: La suma de dos catetos del triangle sempre ha de ser major que la hipotenusa.");
+        }
+        if (cateto1 + hipotenusa <= cateto2) { // Definicion caso 2
+            throw new TriangleImposibleException(
+                    "\nTriangle impossible: La suma del primer catet i la hipotenusa ha de ser major que el segon catet");
+        }
+
+        if (cateto2 + hipotenusa <= cateto1) { // Definicion caso 3
+            throw new TriangleImposibleException(
+                    "\nTriangle impossible: La suma del segon catet i la hipotenusa ha de ser major que el primer catet");
+
         }
 
         this.cateto1 = cateto1;
         this.cateto2 = cateto2;
         this.hipotenusa = hipotenusa;
-
     }
-
 }
